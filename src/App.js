20895bae3aa01695844cd149b3ca2a595f27b0b1
devwithmehar <meharsahil207@gmail.com>
2024-09-21 03:56:54 -0500

    amplify is init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 
 function App() {
   return (
