bf8e8f3e29600355a64b0140aac9c268229fa42f
devwithmehar <meharsahil207@gmail.com>
2024-09-21 05:27:19 -0500

    Dashboard is imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import { Amplify } from 'aws-amplify';
 import amplifyconfig from './amplifyconfiguration.json';
+import Header from "./Components/Header";
+import Dashboard from "./Components/Dashboard";
+import Login from "./Components/Login";
+
+
 Amplify.configure(amplifyconfig);
 
+const code = new URLSearchParams(window.location.search).get("code")
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    return (
+        <>
+        <Header />
+        {code ? <Dashboard code={code} /> : <Login />}
+        </>
+    );
+  
 }
 
 export default App;
