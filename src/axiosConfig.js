dd339d24acaf35ce5d0ada230cc87e2dbd4bb3c0
devwithmehar <meharsahil207@gmail.com>
2024-09-21 05:24:40 -0500

    axios boilerplace is config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://spotify-apis.eba-agwybnxn.us-east-1.elasticbeanstalk.com', 
+  baseURL: process.env.REACT_APP_SERVER_URL , 
   headers: {
     'Content-Type': 'application/json', 
   },
